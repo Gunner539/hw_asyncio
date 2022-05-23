91dffe3444511c25f7bb8fea28179a723a9b2eac
unknown <C:\Users\MINI PC\AppData\Roaming\The Bat!>
2022-05-23 14:57:03 +0300

    Initial commit

---
+++ database.py
@@ -0,0 +1,61 @@
+import asyncio
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from settings import DB_USER, DB_NAME, DB_PASSWORD
+
+Base = declarative_base()
+sw_db = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}'
+
+class StarWarPerson(Base):
+    __tablename__ = 'heroes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    birth_year = Column(String)
+    eye_color = Column(String)
+    films = Column(String)
+    gender = Column(String)
+    hair_color = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    skin_color = Column(String)
+    species = Column(String)
+    starships = Column(String)
+    vehicles = Column(String)
+
+async def save_to_db(data):
+    engine = create_async_engine(sw_db, echo=True,)
+
+    async with engine.begin() as db_con:
+        await db_con.run_sync(Base.metadata.drop_all)
+        await db_con.run_sync(Base.metadata.create_all)
+
+    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+    async with async_session() as session:
+        async with session.begin():
+            for item in data:
+                person = StarWarPerson(
+                    name=item['name'],
+                    birth_year=item['birth_year'],
+                    eye_color=item['eye_color'],
+                    films=item['films'],
+                    gender=item['gender'],
+                    hair_color=item['hair_color'],
+                    height=item['height'],
+                    homeworld=item['homeworld'],
+                    mass=item['mass'],
+                    skin_color=item['skin_color'],
+                    species=item['species'],
+                    starships=item['starships'],
+                    vehicles=item['vehicles'],
+                )
+                session.add(person)
+
+        await session.commit()
+
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
---
+++ main.py
@@ -0,0 +1,68 @@
+import asyncio
+import aiohttp
+import requests
+from database import save_to_db
+import time
+
+
+SW_API = 'https://swapi.dev/api/people/'
+
+
+async def get_hero(url: str):
+    async with aiohttp.client.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+
+def name_generator(url_list):
+    try:
+        for url in url_list:
+            req = requests.get(url)
+            yield req.json()['name']
+    except KeyError:
+        for url in url_list:
+            req = requests.get(url)
+            yield req.json()['title']
+
+
+async def get_homeworld_name(url: str):
+    async with aiohttp.client.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+
+async def get_sw_heroes():
+    sw_persons_list = list()
+    persons_tasks = [get_hero(f'{SW_API}/{i}') for i in range(1, 83)]
+    persons_info = await asyncio.gather(*persons_tasks)
+    for person in persons_info:
+        if person != {'detail': 'Not found'}:
+            hero_data = dict()
+            hero_data['birth_year'] = person['birth_year']
+            hero_data['eye_color'] = person['eye_color']
+            hero_data['films'] = ', '.join(name_generator(person['films']))
+            hero_data['gender'] = person['gender']
+            hero_data['hair_color'] = person['hair_color']
+            hero_data['height'] = person['height']
+            planet = await get_homeworld_name(person['homeworld'])
+            hero_data['homeworld'] = planet['name']
+            hero_data['mass'] = person['mass']
+            hero_data['name'] = person['name']
+            hero_data['skin_color'] = person['skin_color']
+            hero_data['species'] = ', '.join(name_generator(person['species']))
+            hero_data['starships'] = ', '.join(name_generator(person['starships']))
+            hero_data['vehicles'] = ', '.join(name_generator(person['vehicles']))
+            sw_persons_list.append(hero_data)
+    return list(sw_persons_list)
+
+
+async def main():
+    list_charact = await get_sw_heroes()
+    await save_to_db(list_charact)
+
+print('start')
+start = time.time()
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(main())
+print('end')
+print(time.time() - start)
